f592b7a93d113a8c19cbe69173631eab03372e2b
Kajitaka <142304219+kajitaka1201@users.noreply.github.com>
2025-08-31 07:18:32 +0000

    viewport指定

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,31 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Header from "@/components/ui-elements/header";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export const metadata: Metadata = {
   title: "Congestion Checker",
-  description: "喫茶店の混雑を確認するウェブアプリ",
+  description: "喫茶店の混雑を確認するウェブアプリ"
+};
+
+export const viewport: Viewport = {
+  width: 1024
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="ja" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
           <Header />
           {children}
         </ThemeProvider>
