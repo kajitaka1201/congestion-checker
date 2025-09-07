c7f3ea2f4aaf63e0f9bbe6291f387649ce4f43f8
Kajitaka <142304219+kajitaka1201@users.noreply.github.com>
2025-09-07 05:48:17 +0000

    不要な定義の削除

--- src/hooks/generate-desk-style.ts
+++ src/hooks/generate-desk-style.ts
@@ -1,21 +1,19 @@
 import { DeskType } from "@/types/firebase-type";
 
-export interface DeskStyleOptions {
-  containerWidth: number;
-  baseWidth?: number;
-  baseHeight?: number;
-}
-
-export function generateDeskStyle(desk: DeskType, dimensions: { width: number; height: number }) {
-
+export function generateDeskStyle(
+  desk: DeskType,
+  dimensions: { width: number; height: number }
+) {
   const isHorizontal = desk.orientation === "horizontal";
 
-  return isHorizontal ? {
+  return isHorizontal
+    ? {
         top: `${desk.y}%`,
         left: `${desk.x}%`,
         width: dimensions.width * (70 / 900),
         height: dimensions.width * (50 / 900)
-      } : {
+      }
+    : {
         top: `${desk.y}%`,
         left: `${desk.x}%`,
         width: dimensions.width * (50 / 900),
